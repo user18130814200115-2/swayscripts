9ecb5d917d1585af3ff9fb76980d587cca938d5b
user18130814200115-2 <66143190+user18130814200115-2@users.noreply.github.com>
2021-04-18 17:18:53 +0200

    Update fullscreenbarworker.sh

--- fullscreenbarworker/fullscreenbarworker.sh
+++ fullscreenbarworker/fullscreenbarworker.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
-while true; do
-    message=$(swaymsg -t SUBSCRIBE '[ "window" ]') || exit
-    fullscreenmode=$(echo "$message" | grep "change\": \"fullscreen_mode" | grep -o "\"fullscreen_mode\": [0-9]")	
+
+modbar() {
+    fullscreenmode=$(echo "$1" | grep "change\": \"fullscreen_mode" | grep -o "\"fullscreen_mode\": [0-9]")	
 
     if [ -n "$fullscreenmode" ]; then
 	[ ${fullscreenmode##*:} -eq 1 ] && swaymsg bar mode hide
 	[ ${fullscreenmode##*:} -eq 0 ] && swaymsg bar mode dock
     fi
-done
+}
 
+while true; do
+    message=$(swaymsg -t SUBSCRIBE '[ "window" ]') || exit
+    modbar "$message" &
+done
