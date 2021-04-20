2ef30226119f83d15d5c4aa55711ada5943ffe8f
user18130814200115-2 <66143190+user18130814200115-2@users.noreply.github.com>
2021-04-20 14:37:38 +0200

    Update fullscreenbarworker.sh
    Account for closing fullscreened windows

--- fullscreenbarworker/fullscreenbarworker.sh
+++ fullscreenbarworker/fullscreenbarworker.sh
@@ -2,7 +2,8 @@
 
 modbar() {
     fullscreenmode=$(echo "$1" | grep "change\": \"fullscreen_mode" | grep -o "\"fullscreen_mode\": [0-9]")	
-
+    [ -z "$fullscreenmode" ] && fullscreenmode=$(echo "$1" | grep "change\": \"close" | grep -o "\"fullscreen_mode\": [0-9]" | tr "1" "0")
+    
     if [ -n "$fullscreenmode" ]; then
 	[ ${fullscreenmode##*:} -eq 1 ] && swaymsg bar mode hide
 	[ ${fullscreenmode##*:} -eq 0 ] && swaymsg bar mode dock
@@ -13,3 +14,4 @@ while true; do
     message=$(swaymsg -t SUBSCRIBE '[ "window" ]') || exit
     modbar "$message" &
 done
+
