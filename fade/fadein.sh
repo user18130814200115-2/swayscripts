f7eeb4fef9f55ab543e209ff4f50356db1b8247f
user18130814200115-2 <66143190+user18130814200115-2@users.noreply.github.com>
2021-05-08 15:43:22 +0200

    Create fadein.sh

---
+++ fade/fadein.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+id=$(shuf -i 1-9999 -n 1)
+swaymsg '[con_mark=fade]' "mark $id; unmark fade"
+    
+for i in `seq 30`; do
+    sleep 0.001
+    swaymsg "[con_mark=$id]" opacity plus 0.03
+done
+    
+swaymsg "[con_mark=$id]" opacity 1
+swaymsg "[con_mark=$id]" unmark "$id"
