0d816a387a2512f6382067553b54d12529f5fc9f
Froloket64 <ket.frol2006@gmail.com>
2023-03-14 20:42:48 +0300

    Added fps and time parameters; `chmod +x`

--- fade/fadein.sh
+++ fade/fadein.sh
@@ -2,10 +2,19 @@
 
 id=$(shuf -i 1-9999 -n 1)
 swaymsg '[con_mark=fade]' "mark $id"
-    
-for i in `seq 30`; do
-    sleep 0.001
-    swaymsg "[con_mark=$id]" opacity plus 0.03
+
+# TODO: Use args for fps and time values
+FPS=60
+TIME_S=0.1
+
+frames=$(dc -e "$FPS $TIME_S * 1/ p")
+step_time=$(dc -e "3k 1 $FPS / p")
+step_opacity=$(dc -e "3k $step_time $TIME_S / p")
+
+for i in `seq $frames`; do
+    sleep $step_time
+
+    swaymsg "[con_mark=$id]" opacity plus $step_opacity
 done
     
 swaymsg "[con_mark=$id]" opacity 1
